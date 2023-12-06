a58cbe88423c3a71cf3982d613e7b639740c2026
Nurdoidz <nurdoidz@live.com>
2023-12-05 17:47:49 -0700

    Add button to media piece page

--- content_script.js
+++ content_script.js
@@ -2,7 +2,69 @@ console.log('Hello from AniList Not Interested!');
 const animeList = new Set(JSON.parse(localStorage.getItem('notInterestedAnime')));
 const mangaList = new Set(JSON.parse(localStorage.getItem('notInterestedManga')));
 
-const observer = new MutationObserver(() => {
+const rxMatchMediaAndId = new RegExp(/((?:anime)|(?:manga))\/(\d+)/);
+if (rxMatchMediaAndId.test(document.baseURI)) addNotInterestedButton();
+function addNotInterestedButton() {
+
+    if (document.querySelector('.not-interested-button')) return;
+
+    const mediaType = document.baseURI.match(rxMatchMediaAndId)[1];
+    const id = document.baseURI.match(rxMatchMediaAndId)[2];
+    
+    const headerDiv = document.querySelector('.header');
+    const actionsDiv = headerDiv.querySelector('.actions');
+    const buttonDiv = document.createElement('div');
+    buttonDiv.classList.add('not-interested-button');
+
+    const list = mediaType === 'anime' ? animeList : mangaList;
+    let isInterested = !list.has(id);
+    console.log(isInterested);
+    
+    if (isInterested) {
+        buttonDiv.classList.add('add-not-interested');
+        buttonDiv.textContent = 'Not Interested';
+    }
+    else {
+        buttonDiv.classList.add('remove-not-interested');
+        buttonDiv.textContent = 'Interested';
+    }
+    
+    buttonDiv.addEventListener('click', () => {
+        console.log(list);
+        if (isInterested) {
+            list.add(id);
+            buttonDiv.textContent = 'Interested';
+            buttonDiv.classList.add('remove-not-interested');
+            buttonDiv.classList.remove('add-not-interested');
+        }
+        else {
+            list.delete(id);
+            buttonDiv.textContent = 'Not Interested';
+            buttonDiv.classList.add('add-not-interested');
+            buttonDiv.classList.remove('remove-not-interested');
+        }
+        isInterested = !isInterested;
+        const storageKey = mediaType === 'anime' ? 'notInterestedAnime' : 'notInterestedManga';
+        localStorage.setItem(storageKey, JSON.stringify(Array.from(list)));
+        console.log(list);
+    });
+    
+    actionsDiv.appendChild(buttonDiv);
+}
+
+const pageObserver = new MutationObserver((mutations) => {
+    console.log('DOM has changed');
+    mutations.forEach((mutation) => {
+        if (mutation.type === 'childList') {
+            console.log('Child div replaced:', mutation.target);
+        }
+    });
+    if (rxMatchMediaAndId.test(document.baseURI)) addNotInterestedButton();
+});
+
+pageObserver.observe(document.querySelector('.page-content'), { childList: true });
+
+const domObserver = new MutationObserver(() => {
     const covers = document.querySelectorAll('a.cover');
     for (let i = 0; i < covers.length; i++) {
         const path = covers[i].pathname;
@@ -14,4 +76,4 @@ const observer = new MutationObserver(() => {
     }
 });
 
-observer.observe(document, { childList: true, subtree: true });
+domObserver.observe(document.querySelector('.page-content'), { childList: true, subtree: true });
